bb0eae6291dc841890f7e5420e394bfb5f33e21e
Dharmindar <dharmindar@djvsoft.com>
2018-06-30 17:18:41 +0500

    Initial commit

---
+++ wtenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\profiles\dharmindar\clients\djvsoft\wordutopia\wtenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
