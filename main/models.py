5d7b95fd5f2976b596d680d9d3fd4d965cebba4a
vinod_30k <vini@djvsoft.com>
2018-07-09 11:27:57 +0500

    Fields for words are created now in database table.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AddWords(models.Model):
+    ID = models.IntegerField()
+    UserID = models.CharField(max_length=250)
+    Word = models.CharField(max_length=250)
+    Pronunciation = models.CharField(max_length=250)
+    Phrase = models.CharField(max_length=250)
+    Hint = models.CharField(max_length=250)
+    Definition = models.CharField(max_length=500)
+    Synonym = models.CharField(max_length=250)
+    Is_ShareOnline = models.CharField(max_length=250)
+    Is_Favourite = models.CharField(max_length=250)
+    IsSkip = models.CharField(max_length=250)
+    Appearance = models.CharField(max_length=250)
+    WAppreance = models.CharField(max_length=250)
+    Score = models.IntegerField()
+    Addition_Info = models.CharField(max_length=250)
